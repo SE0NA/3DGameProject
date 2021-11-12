4cd4f409f79bdc7fe8d0571b6b2ccdba5459f4b5
SE0NA <85846475+SE0NA@users.noreply.github.com>
2021-11-13 01:46:10 +0900

    옵션 패널 구성
    슬라이더, 데이터초기화, back

---
+++ TheDoor/Assets/Scripts/Main/OptionManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class OptionManager : MonoBehaviour
+{
+    public Slider masterSlider;
+    public Slider bgmSlider;
+    public Slider sfxSlider;
+
+    public GameObject[] activeFalseObejcts;
+    public GameObject optionPanel;
+
+    public AudioMixer myAudio;
+
+    public void Start()
+    {
+     //   masterSlider.value = PlayerPrefs.GetInt("masterAudioMixer");
+     //   bgmSlider.value = PlayerPrefs.GetInt("bgmAudioMixer");
+     //   sfxSlider.value = PlayerPrefs.GetInt("sfxAudioMixer");
+    }
+
+    public void ClickBackBtn()
+    {
+        for(int i=0;i< activeFalseObejcts.Length; i++)
+        {
+            activeFalseObejcts[i].SetActive(true);
+        }
+        optionPanel.SetActive(false);
+    }
+}

d4428fd37e2c92b3e1e30bcd5d8d80a272b35398
SE0NA <85846475+SE0NA@users.noreply.github.com>
2021-11-13 18:36:01 +0900

    옵션 사운드 슬라이더
    슬라이더 구현 및 audioMixer 연결

--- TheDoor/Assets/Scripts/Main/OptionManager.cs
+++ TheDoor/Assets/Scripts/Main/OptionManager.cs
@@ -17,9 +17,37 @@ public class OptionManager : MonoBehaviour
 
     public void Start()
     {
-     //   masterSlider.value = PlayerPrefs.GetInt("masterAudioMixer");
-     //   bgmSlider.value = PlayerPrefs.GetInt("bgmAudioMixer");
-     //   sfxSlider.value = PlayerPrefs.GetInt("sfxAudioMixer");
+        masterSlider.value = PlayerPrefs.GetFloat("masterAudioMixer");
+        bgmSlider.value = PlayerPrefs.GetFloat("bgmAudioMixer");
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxAudioMixer");
+    }
+
+    public void ControlSlider(int sliderNum)
+    {
+        float sound;
+        switch (sliderNum)
+        {
+            case 0: // master
+                sound = masterSlider.value;
+                if (sound == -20f) myAudio.SetFloat("master", -80);
+                else               myAudio.SetFloat("master", sound);
+                PlayerPrefs.SetFloat("masterAudioMixer", sound);
+                break;
+
+            case 1: // bgm
+                sound = bgmSlider.value;
+                if (sound == -20f) myAudio.SetFloat("bgm", -80);
+                else myAudio.SetFloat("bgm", sound);
+                PlayerPrefs.SetFloat("bgmAudioMixer", sound);
+                break;
+
+            case 2: // sfx
+                sound = sfxSlider.value;
+                if (sound == -20f) myAudio.SetFloat("sfx", -80);
+                else myAudio.SetFloat("sfx", sound);
+                PlayerPrefs.SetFloat("sfxAudioMixer", sound);
+                break;
+        }
     }
 
     public void ClickBackBtn()
